a42a0772aa10027692e3370c91de1e25f296d2b1
Salvatore Iovene <salvatore.iovene@intel.com>
2016-01-28 11:28:29 +0200

    Add findResources to OICService

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,9 +1,31 @@
 angular.module('oic_demo.services', [])
 
-.factory('OICService', function() {
+.factory('OICService', function($ionicPlatform) {
     var _service = {
+        plugin: null,
         resources: []
     };
 
-    return _service;
+    function _onresourcefound(event) {
+        resource.push(event.resource);
+    }
+
+    function _findResources(options) {
+        return _service.plugin.findResources(options);
+    }
+
+    // Init
+    $ionicPlatform.ready(function() {
+        _service.plugin = cordova.require('cordova/plugin/oic');
+        _service.plugin.onresourcefound = _onresourcefound;
+        _findResources();
+    });
+
+    return {
+        // Data
+        resources: _service.resourcess,
+
+        // Functions
+        findResources: _findResources
+    };
 });
