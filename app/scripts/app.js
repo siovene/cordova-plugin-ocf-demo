28707a533ba46bacbadcc9c9cdecdf798e0716e7
Salvatore Iovene <salvatore.iovene@intel.com>
2016-01-28 10:20:50 +0200

    Rename the app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,10 @@
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'oic_demo' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+// 'oic_demo.controllers' is found in controllers.js
+angular.module('oic_demo', ['ionic', 'oic_demo.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('oic_demo.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
